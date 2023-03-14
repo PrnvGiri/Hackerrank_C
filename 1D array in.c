c0dabbc24ff15e484295d74339b170c723086a6d
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2023-03-14 23:50:33 +0530

    Create 1D array in.c

---
+++ 1D array in.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+       for(i=0;i<n;i++)
+       {
+           scanf("%d",&arr[i]);
+           sum=sum+arr[i];
+       }
+       printf("%d ",sum);
+    return 0;
+}
